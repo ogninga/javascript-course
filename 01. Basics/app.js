3485f2281ee6d890408cc980faa0dadca78eca6b
CLyon <66389401+ogninga@users.noreply.github.com>
2023-11-29 03:40:25 -0500

    Initial commit

---
+++ 01. Basics/app.js
@@ -0,0 +1,256 @@
+// let firstFavNumb = 36;
+// let secondFavNumb = 14;
+// console.log(firstFavNumb > secondFavNumb);
+// console.log(firstFavNumb < secondFavNumb);
+// console.log(firstFavNumb >= secondFavNumb);
+// console.log(firstFavNumb <= secondFavNumb);
+// console.log(firstFavNumb === secondFavNumb);
+// console.log(firstFavNumb == secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+// console.log(firstFavNumb != secondFavNumb);
+
+// let favActorFirstName = "Timmy ";
+// let favActorLastName = "Jones";
+
+// let fullActorName = favActorFirstName.concat(favActorLastName);
+
+// let uppercase = fullActorName.toUpperCase();
+// let message = 'My favorite actor is ' + uppercase + 'noodle boodlle';
+// console.log(message += ' best show is nothing more than ' + fullActorName    );
+
+
+// let password; 
+
+// if (password === 8) {
+//     console.log('Welcome!'); 
+// } else if (password <= 8 ){
+//     console.log('Password must be at least 8 characters');
+// } else if (password >= 8){
+//     console.log('Password is too long and should be at least 8 characters');
+// } else {console.log('Please enter a valid password')}
+
+
+// let x = 1
+// let text;
+
+
+// switch (x) {
+
+//     case 0:
+//         text = "off"
+//         console.log(text);
+//         break;
+//     case 1:
+//         text = "On"
+//         console.log(text);
+//         break;
+//     default:
+//         text = 'no value found';
+//         console.log(text);
+    
+// }
+
+
+// let fruit = "banana";
+
+// switch (fruit){
+//     case "banana":
+//         text = "Banana is good to eat";
+//         console.log(text);
+//         break;
+//     case "Orange":
+//         text = "I am not a fan of orange";
+//         console.log(text);
+//         break;
+//     case "Apple":
+//         text = "How you like them apples";
+//         console.log(text);
+//         break;
+//     default:
+//         text = "tf is that?";
+//         console.log(text);
+// }
+
+// for ( let i = 0; i <= 15; i++){
+//     console.log("What it do", i);
+// }
+
+
+// let name = "timmy";
+
+// for ( let i = 0; i <= 15; i++){
+//     console.log(name, i)
+// }
+
+
+// let i = 1;
+
+// while (i <= 5){
+//     console.log("what does it does  you know");
+//     i++
+// }
+//  let i = 10;
+
+//  while (i <= 100){
+//     console.log("e");
+// i++
+
+//  }
+
+
+// let i = 1;
+
+// do {
+//     console.log("hellllll", i);
+//     i++;
+// } while (i <= 5);
+
+let i = 20
+
+do {
+    console.log("hellllll", i);
+    i++
+} while ( i <= 400);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
