d168c8ef8e2b3f8afb00cc7833335c5530ba40f5
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-05 03:26:27 -0500

    Update app.js

--- 01. Basics/app.js
+++ 01. Basics/app.js
@@ -687,72 +687,157 @@
 
 //destructuring mixer Read over this to see the limits of dustructuring and refer back to this in the future.
 
-const data = {
-    user:{
-        id:123,
-        name: "John Doe",
-        age: 30,
-        email: "john.doe@example.com",
-        address: {
-            city:"New York",
-            country: 'USA',
-        },
-        hobbies: ["Reading", "Painting", "Cooking"],
-        scores: {
-            math:95,
-            science: 88,
-            history: 75,
-        },
-    },
-    products: [
-        {id: 1, name: "Laptop", price: 1000},
-        {id: 2, name: "Phone", price: 800},
-        {id: 3, name: "Tablet", price: 500},
-    ],
-    settings: {
-        darkMode: true,
-        notifications: {
-            email: true,
-            sms: false,
-            push: true,
-        },
-        language: "English",
-    },
-};
+// const data = {
+//     user:{
+//         id:123,
+//         name: "John Doe",
+//         age: 30,
+//         email: "john.doe@example.com",
+//         address: {
+//             city:"New York",
+//             country: 'USA',
+//         },
+//         hobbies: ["Reading", "Painting", "Cooking"],
+//         scores: {
+//             math:95,
+//             science: 88,
+//             history: 75,
+//         },
+//     },
+//     products: [
+//         {id: 1, name: "Laptop", price: 1000},
+//         {id: 2, name: "Phone", price: 800},
+//         {id: 3, name: "Tablet", price: 500},
+//     ],
+//     settings: {
+//         darkMode: true,
+//         notifications: {
+//             email: true,
+//             sms: false,
+//             push: true,
+//         },
+//         language: "English",
+//     },
+// };
 
-const {
-    user: {
-        name, age, address: {city, country},hobbies,scores: {math, science, history},
-        email,
+// const {
+//     user: {
+//         name, age, address: {city, country},hobbies,scores: {math, science, history},
+//         email,
 
-    },
-    products: [product1, product2, product3],
-    settings:{
-        darkMode,
-        notifications:{
-            email: emailNotification,
-            sms: smsNotification,
-            push: PushNotification,
-        },
-        language,
-    },
-} = data;
+//     },
+//     products: [product1, product2, product3],
+//     settings:{
+//         darkMode,
+//         notifications:{
+//             email: emailNotification,
+//             sms: smsNotification,
+//             push: PushNotification,
+//         },
+//         language,
+//     },
+// } = data;
 
-console.log(`Name: ${name}`);
-console.log(`Age: ${age}`);
-console.log(`Address: ${city}, ${country}`);
-console.log(`Hobbies: ${hobbies.join(", ")}`);
-console.log(`Math Score: ${math}`);
-console.log(`Science Score: ${science}`);
-console.log(`History Score: ${history}`);
-console.log(`Product1: ${product1.name} - $${product1.price}`);
-console.log(`Product2: ${product2.name} - $${product2.price}`);
-console.log(`Product3: ${product3.name} - $${product3.price}`);
-console.log(`Dark Mode: ${darkMode}`);
-console.log(`Email Notification: ${emailNotification}`);
-console.log(`SMS Notification: ${smsNotification}`);
-console.log(`Push Notification: ${PushNotification}`);
-console.log(`language: ${language}`);
+// console.log(`Name: ${name}`);
+// console.log(`Age: ${age}`);
+// console.log(`Address: ${city}, ${country}`);
+// console.log(`Hobbies: ${hobbies.join(", ")}`);
+// console.log(`Math Score: ${math}`);
+// console.log(`Science Score: ${science}`);
+// console.log(`History Score: ${history}`);
+// console.log(`Product1: ${product1.name} - $${product1.price}`);
+// console.log(`Product2: ${product2.name} - $${product2.price}`);
+// console.log(`Product3: ${product3.name} - $${product3.price}`);
+// console.log(`Dark Mode: ${darkMode}`);
+// console.log(`Email Notification: ${emailNotification}`);
+// console.log(`SMS Notification: ${smsNotification}`);
+// console.log(`Push Notification: ${PushNotification}`);
+// console.log(`language: ${language}`);
+
+//ternary operator short hand for if else, doesnt help an else if statement
+
+// condition ? exprIfTrue : exprIfFalse;
+
+// let password = 8;
+
+// passwordChecker = ps =>{
+//     if (ps === 8) {
+//         return `Strong Password`
+//     } else {
+//         return "Password should be 8 characters."
+//     }
+// }
+
+//now with ternary operator
+
+// passwordChecker = ps =>{
+//    return ps === 8 ? 'Strong Password' : "Password should be 8 characters"
+// }
+
+// const res = passwordChecker(password)
+
+// console.log(res)
+
+// second ternary operator example
+// const age = 25
+// const isAdult = age >= 18 ? "adult" : "Not an Adult"
+
+// console.log(isAdult)
+
+// const walletAmount = 1
+
+// MoneyChecker = mc =>{
+//     return mc >= 1 ? "Are able to buy things" : "Not able to buy anything"
+// }
+
+// const storeMinimumMoney = MoneyChecker(walletAmount)
+// console.log(storeMinimumMoney)
+
+// // or
+
+// const storeMinimum = walletAmount>= 1 ? "Sure come on in you got some money" : "Go away, you have no money"
+// console.log(storeMinimum)
+
+// for in loops
+
+// for (let/const key in object) {...}
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     gender: "Male",
+// }
+//  for (let keys in person) {
+//     console.log(keys, person[keys])
+//  }
+ // you can write the varible name next to chosen value in key object and it will be returned
+
+
+// let list = ['one', 'two', 'three', 'four']
+
+// for (let index in list){
+//     console.log(`${index}: ${list[index]}`)
+//     console.log(index, list[index])
+// }
+
+const object = {a: 1, b:2, c:3};
+
+for (let list in object){
+    console.log(list, object[list])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
