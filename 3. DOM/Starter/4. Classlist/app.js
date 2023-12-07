2ca89c0f67b191b0dd4dda58b55183f8e665b52b
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-07 06:52:52 -0500

    finished more exercises

--- 3. DOM/Starter/4. Classlist/app.js
+++ 3. DOM/Starter/4. Classlist/app.js
@@ -1,4 +1,23 @@
-// classlist
+// classlist 
+
+
+
 // add()
 // remove()
 // toggle()
+
+
+
+const h1 = document.querySelector('h1');
+// console.log(h1.classList);
+
+// h1.classList.add("styles")
+// h1.classList.remove("styles")
+// console.log(h1.classList)
+
+h1.classList.toggle("styles")
+
+
+
+
+
--- 3. DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ 3. DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -2,4 +2,18 @@
 // value
 // type
 // getAttribute(attrName)
-// setAttribute(attrName, value)
+// setAttribute(attrName, value) 
+
+const a = document.querySelector("a")
+const input = document.querySelector("input")
+
+// console.log(a.href = 'www.youtube.com')
+// console.log(input.value)
+// console.log(input.type)
+
+console.log(input.getAttribute('type'))
+
+//setAttribute
+
+input.setAttribute('placeholder', "password")
+
