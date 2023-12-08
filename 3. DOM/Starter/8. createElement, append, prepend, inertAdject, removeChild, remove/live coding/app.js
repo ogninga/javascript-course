524ec11ec6a3b0b214b9612715b92e04d7c1fa8e
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-07 20:54:05 -0500

    append etc notes done.

--- 3. DOM/Starter/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ 3. DOM/Starter/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -1,9 +1,50 @@
 // ------------------------------------
 // createElement()
+
+//creating element
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "who"
+// h1.classList.add("greetings")
+// console.log(h1.textContent)
+
+
+
+
+
+
+
 // appendChild()
+
+// const body = document.body;
+// body.appendChild(h1)
+
+
+
+
+
 // append()
 // prepend()
 // insertBefore()
+
+// const ul = document.querySelector("ul")
+
+// const newLi = document.createElement("li")
+// newLi.innerText= "i'm li tag"
+
+// ul.appendChild(newLi)
+
+// const firstLi = document.querySelector("li")
+
+//selector.insertBefore(what, where)
+
+// selector.insertBefore(newLi, firstLi)
+
+
+
+
+
+
 
 /* insertAdjacentElement(
     'beforebegin'
@@ -16,3 +57,31 @@
 // removeChild()
 // remove()
 // ------------------------------------
+
+// const firstP = document.querySelector("p")
+// const i = document.createElement("i")
+// i.innerText = "i'm italics"
+// i.style.color = "skyblue"
+// firstP.insertAdjacentElement("beforebegin", i)
+// firstP.insertAdjacentElement("afterbegin", i)
+// firstP.insertAdjacentElement("beforeend", i)
+// firstP.insertAdjacentElement("afterend", i)
+
+// let section = document.querySelector('section')
+// const i = document.createElement("i")
+// i.innerText = "i'm italics"
+// i.style.color = "skyblue"
+// const h4 = document.createElement("h4")
+// h4.innerText = "poo"
+// h4.style.color ="crimson"
+
+
+// section.append(i)
+// section.prepend(i)
+
+// const newList = document.querySelector(".new-list")
+
+// const fourth = document.querySelector(".fourth")
+// newList.removeChild(fourth)
+
+
