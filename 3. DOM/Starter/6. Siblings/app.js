76780e3d50a9715284648b320d70959e29512ec3
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-07 19:46:02 -0500

    learned siblings

---
+++ 3. DOM/Starter/6. Siblings/app.js
@@ -0,0 +1,42 @@
+// let firstLi = document.querySelector("li")
+
+// console.log(firstLi.parentElement.parentElement)
+
+//children
+// let ul = document.querySelector("ul")
+// console.log(ul.children)
+// console.log(ul.children[0])
+// console.log(ul.children[1])
+// console.log(ul.children[2])
+// console.log(ul.children[3])
+
+// console.log(ul.children[0].innerText = "One")
+
+// let firstLi = document.querySelector("li")
+//get sibling
+// console.log(firstLi.nextElementSibling.textContent)
+
+
+// let fourthLi = document.querySelector(".fourth")
+// console.log(fourthLi.previousElementSibling)
+// console.log(fourthLi.previousElementSibling.previousElementSibling)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
