8ab140aebc4006a17e2736e0924d11e0f8a1a080
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-07 22:04:28 -0500

    finished key and mouse events, added project folder

--- 3. DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
+++ 3. DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,38 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+
+// input.addEventListener("keypress", () => console.log("keypressed"))
+
+// input.addEventListener("keyup", () => console.log("keypressed"))
+
+// input.addEventListener("keydown", () => console.log("keypressed"))
+
+//USEFUL PROPERTIES AND METHODS
+
+input.addEventListener('keypress', e =>{
+    // console.log(e.charCode)
+    // console.log(e.code)
+    // console.log(e.ctrlKey)
+    // console.log(e.key)
+    // console.log(e.shiftKey)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
