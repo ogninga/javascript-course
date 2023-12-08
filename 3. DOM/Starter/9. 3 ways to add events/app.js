cf186a9e62263676be366a3882646bf74001c740
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-07 21:42:01 -0500

    did event listeners part 1

--- 3. DOM/Starter/9. 3 ways to add events/app.js
+++ 3. DOM/Starter/9. 3 ways to add events/app.js
@@ -1,5 +1,38 @@
 // ----------- BAD WAY-----------
+// never use inline events
+
+// const secondBtn = document.querySelector(".second-btn")
+// secondBtn.onclick = () => console.log("lyon on stone")
+
+
 
 // ----------- GREAT WAY-----------
 
+// const best = document.querySelector(".best")
+
+// const hello =  () => {console.log("hello World")}
+
+
+
+// best.addEventListener("click", () =>  console.log("yellow") )
+
+
 // ----------- Event (e) Object -----------
+
+
+// const para = document.querySelector(".para")
+
+// para.addEventListener("click",(event) =>(console.log(event)))
+
+//preventDefault() stop default behavior
+
+
+
+
+
+
+
+
+
+
+
