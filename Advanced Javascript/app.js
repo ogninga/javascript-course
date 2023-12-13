de0a3e6d99c9d52277a4b32568b53ff96f776fcd
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-13 01:07:53 -0500

    get data from json file learned.

--- Advanced Javascript/app.js
+++ Advanced Javascript/app.js
@@ -1006,6 +1006,74 @@
 
 // console.log(fetch('text.txt'))
 
+//retrieve api from json file.
+
+const result = document.querySelector('.results')
+async function renderData(){
+    try{
+        const response = await fetch('data.json')
+        if(!response.ok) throw Error(response.statusText)
+        const data = await response.json()
+    //result.textContent = data.name
+    result.textContent = data.age
+    console.log(data)
+
+    } catch (error){
+        console.log(error)
+    }
+}
+
+
+renderData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
