76e7bd7cfa9f52526b3350b3a8de6474fa82a950
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-16 10:29:22 -0500

    learned varibles and type inferring

---
+++ typescript course/index.ts
@@ -0,0 +1,26 @@
+//strings
+
+// let myName: string = "Jose"; //set value
+// myName = "Jose"; //change value
+// console.log(myName);
+//numbers
+// let favNumber: number = 10;
+// console.log(favNumber);
+//boolean
+// let tsHard: boolean = false;
+// tsHard = true;
+// console.log(tsHard);
+
+//type inferring assumes the type based on value
+
+// let tech = "Typescript";
+// cant change type, ie tech to boolean or number and any other way
+// let favNumber = 10;
+// let love = true;
+// console.log(typeof tech);
+// console.log(typeof favNumber);
+// console.log(typeof love);
+
+// any type, can be used to reference any type.
+
+// warning should be used rarely. Overuse of any can lead to untyped code, and make it much harder to catch bugs during development related to type. it is better to use specific types when possible to keep benefits of type checking.
