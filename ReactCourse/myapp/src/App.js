44338d276b5c4d5faf68856c734d707dd9c94ea1
CLyon <66389401+ogninga@users.noreply.github.com>
2023-12-21 12:53:43 -0500

    learned Props

--- ReactCourse/myapp/src/App.js
+++ ReactCourse/myapp/src/App.js
@@ -1,31 +1,33 @@
-function App() {
-  const userInfo = [
-    {
-      username: "Leslie",
-      email: "test@gmail.com",
-      location: "UK",
-    },
-    {
-      username: "pookie",
-      email: "test@gmail.com",
-      location: "Uganda",
-    },
-    {
-      username: "ray ray",
-      email: "test@gmail.com",
-      location: "Usa",
-    },
-  ];
+// must use special word children
 
+const User = ({ img, name, hobbies, children }) => {
+  console.log(name);
+  return (
+    <section>
+      <img src={img} alt={name} />
+      <h1> name: {name}</h1>
+      <h3>hobbies: {hobbies}</h3>
+      {children}
+    </section>
+  );
+};
+// made the user component and open and closing tag, putting the data in user kinda like an html document, and between the tags the children
+function App() {
   return (
     <>
-      {userInfo.map((user) => (
-        <ul key={Math.random() * 10}>
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>{user.location}</li>
-        </ul>
-      ))}
+      <User
+        img="https://th.bing.com/th/id/OIP.B29P-JotxZYlRA7smM4ghAHaE8?rs=1&pid=ImgDetMain"
+        name="John"
+        age={18}
+        hobbies={["coding ", "reading ", "sleeping "]}
+        realData={{ name: "alex", location: "new york" }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est harum
+          repellendus quia dolorem doloribus consequatur error laudantium
+          delectus ab mollitia.
+        </p>
+      </User>
     </>
   );
 }
